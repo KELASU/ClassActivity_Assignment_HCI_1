5b492f77103bd0cad8844ec2fb0df4ea3162ddc9
KELASU <techmaker24@gmail.com>
2022-12-20 14:42:08 +0700

    Update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//Please put your answer here
+$("#close-button").click(function(){
+    $("#signup-modal").hide(100);
+})
+
+$(".singup-show").click(function(){
+    $("#signup-modal").show(100);
+})
